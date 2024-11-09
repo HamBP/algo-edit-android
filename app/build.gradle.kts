e1cacd0449bde0a050655c2f08b193f2576e105f
algosketch <algosketch@gmail.com>
2024-11-09 16:14:49 +0900

    chore : 내비게이션 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,11 +60,15 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.ffmpeg.kit.full)
+    implementation(libs.androidx.navigation.compose)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
